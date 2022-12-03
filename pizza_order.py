e621a1aae324f0f517e960e0582325d5f0739490
dyushakolesov <dyushakolesov@mail.ru>
2022-12-03 20:51:49 +0300

    fixed issues

--- pizza_order.py
+++ pizza_order.py
@@ -1,28 +1,36 @@
-from time import sleep
 import click
-from random import randint
 import sys
+from random import randint
+from time import sleep
 from typing import Union, Callable
+from functools import wraps
+from click_option_group import optgroup, MutuallyExclusiveOptionGroup
 
 
 def str_to_class(str: str):
     """Функция для перевода из строкового названия пиццы в консоли в объект класса"""
-    return getattr(sys.modules[__name__], str.title())
+    try:
+        return getattr(sys.modules[__name__], str.title())
+    except (AttributeError):
+        print("Wrong pizza name, order again")
+        return
 
 
 def log(msg: str):
     """Выводит время выполниния функции, принимая соответствующий шаблон"""
 
-    def _log(func: Callable):
+    @wraps(msg)
+    def inner_log_func(func: Callable):
+        @wraps(func)
         def wrapper(pizza: Pizza):
-            attr = func.__name__ + "time"
+            attr = func.__name__ + "_time"
             f_time = getattr(pizza, attr)
             sleep(f_time)
             print(msg.format(f_time))
 
         return wrapper
 
-    return _log
+    return inner_log_func
 
 
 @click.group()
@@ -34,7 +42,9 @@ def cli():
 def menu():
     """Выводит меню"""
     print(
-        "— Margherita 🧀 : tomato sauce, mozzarella, tomatoes\n— Pepperoni 🍕 : tomato sauce, mozzarella, pepperoni\n— Hawaiian 🍍 : tomato sauce, mozzarella, chicken, pineapples"
+        "— Margherita 🧀 : tomato sauce, mozzarella, tomatoes\n"
+        "— Pepperoni 🍕 : tomato sauce, mozzarella, pepperoni\n"
+        "— Hawaiian 🍍 : tomato sauce, mozzarella, chicken, pineapples"
     )
 
 
@@ -53,7 +63,8 @@ class Recipe:
 
 class Pizza:
     """Класс для описания пицц:
-    - bake_time, delivery_time и pickup_time определяют время готовки, доставки, самовывоза
+    - pizza_bake_time, pizza_delivery_time и pizza_pickup_time
+      определяют время готовки, доставки, самовывоза
     - При задании размера XL пицца готовится на 2 сек дольше
     - Рецепт пиццы передается в формате: название пиццы, список ингридиентов
     """
@@ -64,15 +75,15 @@ class Pizza:
     recipe: Recipe
     recipe_list: list
 
-    bake_time = randint(1, 5)
-    delivery_time = randint(1, 4)
-    pickup_time = randint(1, 4)
+    pizza_bake_time = randint(1, 5)
+    pizza_delivery_time = randint(1, 4)
+    pizza_pickup_time = randint(1, 4)
 
     def __init__(self, size="L") -> None:
         self.size = size
         if size == "XL":
-            self.bake_time += 2
-        name = type(self).__name__ + " " + self.emoji
+            self.pizza_bake_time += 2
+        name = type(self).__name__ + " {}".format(self.emoji)
         self.recipe = Recipe(name, self.recipe_list)
 
 
@@ -92,38 +103,41 @@ class Hawaiian(Pizza):
 
 
 @log("👨‍🍳 Приготовили за {}с!")
-def bake_(pizza: Pizza):
+def pizza_bake(pizza: Pizza):
     """Готовит пиццу"""
     pass
 
 
 @log("🛵 Доставили за {}с!")
-def delivery_(pizza: Pizza):
+def pizza_delivery(pizza: Pizza):
     """Доставляет пиццу"""
     pass
 
 
 @log("🏠 Забрали за {}с!")
-def pickup_(pizza: Pizza):
+def pizza_pickup(pizza: Pizza):
     """Самовывоз"""
     pass
 
 
 @cli.command()
-@click.option("--delivery", default=False, is_flag=True)
-@click.option("--pickup", default=False, is_flag=True)
+@optgroup.group("Mutally exclusive flags", cls=MutuallyExclusiveOptionGroup)
+@optgroup.option("--delivery", default=False, is_flag=True)
+@optgroup.option("--pickup", default=False, is_flag=True)
 @click.argument("pizza", nargs=1)
 def order(pizza: Union[str, Pizza], delivery: bool, pickup: bool):
     """Готовит и доставляет пиццу"""
     if isinstance(pizza, str):
         pizza_obj = str_to_class(pizza.title())
+        if not pizza_obj:
+            return
     else:
         pizza_obj = pizza
-    bake_(pizza_obj)
+    pizza_bake(pizza_obj)
     if delivery:
-        delivery_(pizza_obj)
+        pizza_delivery(pizza_obj)
     if pickup:
-        pickup_(pizza_obj)
+        pizza_pickup(pizza_obj)
 
 
 if __name__ == "__main__":
--- test_pizza_order.py
+++ test_pizza_order.py
@@ -1,57 +1,56 @@
-from pizza_order import *
-import builtins
 import pytest
-from unittest.mock import Mock
-import contextlib, io
+from pizza_order import *
 from click.testing import CliRunner
 
-
-@pytest.mark.parametrize("test_pizza_obj", [Pepperoni(), Margherita(), Hawaiian()])
-@pytest.mark.parametrize("test_func", [pickup_, delivery_, bake_])
-def test_log_output(test_pizza_obj, test_func):
-    """Тест для всех типов пицц для всех основных методов"""
-    mock = Mock()
-    mock.side_effect = print  # ensure actual print is called to capture its txt
-    print_original = print
-    builtins.print = mock
-    expected_out = ""
-    try:
-        str_io = io.StringIO()
-        with contextlib.redirect_stdout(str_io):
-            test_func(test_pizza_obj)
-        output = str_io.getvalue()
-        match test_func.__name__:
-            case "pickup_":
-                expected_out = "🏠 Забрали за {}с!"
-            case "delivery_":
-                expected_out = "🛵 Доставили за {}с!"
-            case "bake_":
-                expected_out = "󰳏 Приготовили за {}с!"
-        expected_str = expected_out.format(test_pizza_obj.pickup_time)
-        assert print.called  # `called` is a Mock attribute
-        assert output.startswith(expected_str)  # check what function prints
-    finally:
-        builtins.print = print_original  # ensure print is "unmocked"
+PIZZA_OBJ_LIST = [Pepperoni(), Margherita(), Hawaiian()]
+PIZZA_FUNCS_LIST = [pizza_pickup, pizza_delivery, pizza_bake]
 
 
-@pytest.mark.parametrize("test_pizza_obj", [Pepperoni(), Margherita(), Hawaiian()])
+@pytest.mark.parametrize("test_pizza_obj", PIZZA_OBJ_LIST)
+@pytest.mark.parametrize("test_func", PIZZA_FUNCS_LIST)
+def test_log_output(capsys, test_pizza_obj, test_func):
+    """Тест для всех типов пицц для всех основных методов (кроме order)"""
+    function_name = test_func.__name__
+    attr_name = function_name + "_time"
+    test_func(test_pizza_obj)
+    captured = capsys.readouterr()
+    match function_name:
+        case "pizza_pickup":
+            expected_out = "🏠 Забрали за {}с!"
+        case "pizza_delivery":
+            expected_out = "🛵 Доставили за {}с!"
+        case "pizza_bake":
+            expected_out = "👨‍🍳 Приготовили за {}с!"
+    expected_str = expected_out.format(getattr(test_pizza_obj, attr_name))
+
+    assert captured.out.startswith(expected_str)
+
+
+@pytest.mark.parametrize("test_pizza_obj", PIZZA_OBJ_LIST)
 def test_dict_result(test_pizza_obj):
     """Тест для проверки результата метода __dict__ для рецепта пиццы"""
     match test_pizza_obj:
         case Pepperoni():
             expected_res = {
                 "pizza_name": "Pepperoni 🍕",
-                "recipe_list": ["tomato sauce", "mozzarella", "pepperoni"],
+                "recipe_list": ["tomato sauce",
+                                "mozzarella",
+                                "pepperoni"],
             }
         case Margherita():
             expected_res = {
                 "pizza_name": "Margherita 🧀",
-                "recipe_list": ["tomato sauce", "mozzarella", "tomatoes"],
+                "recipe_list": ["tomato sauce",
+                                "mozzarella",
+                                "tomatoes"],
             }
         case Hawaiian():
             expected_res = {
                 "pizza_name": "Hawaiian 🍍",
-                "recipe_list": ["tomato sauce", "mozzarella", "chicken", "pineapples"],
+                "recipe_list": ["tomato sauce",
+                                "mozzarella",
+                                "chicken",
+                                "pineapples"],
             }
     assert test_pizza_obj.recipe.__dict__ == expected_res
 
@@ -71,51 +70,55 @@ def test_avg_time(test_pizza_obj, test_pizza_obj_big):
     large_pizza_time = 0
     extra_large_pizza_time = 0
     for _ in range(OBJ_NUM):
-        large_pizza_time += test_pizza_obj.bake_time
-        extra_large_pizza_time += test_pizza_obj_big.bake_time
+        large_pizza_time += test_pizza_obj.pizza_bake_time
+        extra_large_pizza_time += test_pizza_obj_big.pizza_bake_time
 
     assert extra_large_pizza_time / OBJ_NUM > large_pizza_time / OBJ_NUM
 
 
 @pytest.mark.parametrize("test_pizza_obj", ["pepperoni", "margherita", "hawaiian"])
-@pytest.mark.parametrize("delivery", [True, False])
-@pytest.mark.parametrize("pickup", [True, False])
-def test_order_output(test_pizza_obj, delivery, pickup):
-    """Тест для всех типов пицц для всех метода order"""
+def test_order_output(test_pizza_obj):
+    """Тест для всех типов пицц метода order"""
     runner = CliRunner()
-    mock = Mock()
-    mock.side_effect = print  # ensure actual print is called to capture its txt
-    print_original = print
-    builtins.print = mock
+    expected_out = "👨‍🍳 Приготовили за {}с!"
+
+    result = runner.invoke(order, [test_pizza_obj])
+    test_pizza_obj = str_to_class(test_pizza_obj)
+    expected_str = expected_out.format(test_pizza_obj.pizza_bake_time)
+    assert result.exit_code == 0
+    assert expected_str in result.output  # check what function prints
+
+
+@pytest.mark.parametrize("test_pizza_obj", ["pepperoni", "margherita", "hawaiian"])
+def test_order_output_with_delivery(test_pizza_obj):
+    """Тест для всех типов пицц метода order c флагом --delivery"""
+    runner = CliRunner()
     expected_out = "👨‍🍳 Приготовили за {}с!"
     expected_delivery_out = "🛵 Доставили за {}с!"
-    expected_pickup_out = "🏠 Забрали за {}с!"
-    try:
-        if delivery and pickup:
-            print("You can't pickup and get delivery at same time")
-            return
-        if delivery:
-            result = runner.invoke(order, [test_pizza_obj, "--delivery"])
-        elif pickup:
-            result = runner.invoke(order, [test_pizza_obj, "--pickup"])
-        else:
-            result = runner.invoke(order, [test_pizza_obj])
-        test_pizza_obj = str_to_class(test_pizza_obj)
-        expected_str = expected_out.format(test_pizza_obj.bake_time)
-        assert result.exit_code == 0
-        assert print.called  # `called` is a Mock attribute
-        assert expected_str in result.output  # check what function prints
-        if delivery:
-            assert (
-                expected_delivery_out.format(test_pizza_obj.delivery_time)
-                in result.output
-            )  # check what function prints
-        if pickup:
-            assert (
-                expected_pickup_out.format(test_pizza_obj.pickup_time) in result.output
-            ), "No pickup data in output: " + str(
-                result.output
-            )  # check what function prints
 
-    finally:
-        builtins.print = print_original  # ensure print is "unmocked"
+    result = runner.invoke(order, [test_pizza_obj, "--delivery"])
+    test_pizza_obj = str_to_class(test_pizza_obj)
+    expected_str = expected_out.format(test_pizza_obj.pizza_bake_time)
+    assert result.exit_code == 0
+    assert expected_str in result.output
+    assert (
+        expected_delivery_out.format(test_pizza_obj.pizza_delivery_time)
+        in result.output
+    ), "No delivery data in output{}: ".format(result.output)
+
+
+@pytest.mark.parametrize("test_pizza_obj", ["pepperoni", "margherita", "hawaiian"])
+def test_order_output_with_pickup(test_pizza_obj):
+    """Тест для всех типов пицц для метода order с флагом --pickup"""
+    runner = CliRunner()
+    expected_out = "👨‍🍳 Приготовили за {}с!"
+    expected_pickup_out = "🏠 Забрали за {}с!"
+
+    result = runner.invoke(order, [test_pizza_obj, "--pickup"])
+    test_pizza_obj = str_to_class(test_pizza_obj)
+    expected_str = expected_out.format(test_pizza_obj.pizza_bake_time)
+    assert result.exit_code == 0
+    assert expected_str in result.output
+    assert (
+        expected_pickup_out.format(test_pizza_obj.pizza_pickup_time) in result.output
+    ), "No pickup data in output{}: ".format(result.output)
